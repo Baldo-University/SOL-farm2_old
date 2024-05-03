701082db19eda5d5f18d9df04d67523820219819
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-05-03 11:21:45 -0400

    Add files via upload

--- collector.c
+++ collector.c
@@ -3,8 +3,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/select.h> 
 
 #define UNIX_MAX_PATH 108
+#define N 255	//lunghezza massima di un filepath 
 
 #define ec_minusone(s,m) \
 	if((s)==-1) {perror(m); exit(EXIT_FAILURE);}
@@ -30,13 +32,22 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	/*Setup socket*/
+	int running=1;	//int che fa da booleano per il while
 	int fd_skt, fd_c;	//file descriptor di socket di ascolto passivo e di 
+	fd_set set, rdset;	//insieme di fd attivi e ins. di fd attesi in lettura 
+	int nread;
 	struct sockaddr_un sa;
 	strncpy(sa.sun_path,argv[1],UNIX_PATH_MAX);	//copia nel sockaddr il pathname del socket
 	sa.sun_family=AF_UNIX;
 	fd_skt=socket(AF_UNIX,SOCK_STREAM,0);	//creazione socket
 	ec_minusone(fd_skt,"Collector, creazione socket");
+	//dare nome alla socket
 	ec_minusone(bind(fd_skt,(struct sockaddr *)&sa,sizeof(sa)),"Collector, bind");
-	//in while? 
-	ec_minusone(listen(fd_skt,SOMAXCONN),"Collector, listen");
+	ec_minusone(listen(fd_skt,SOMAXCONN),"Collector, listen");	//segna socket in ascolto
+	FD_ZERO(&set);
+	FD_SET(fd_skt,&set);
+	while(running) {
+		rdset=set;
+		
+	}
 }
