04a48e0d1460c7771920e531e3e2d27e251230c3
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-05-09 06:17:34 -0400

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,9 @@
+#ifndef QUEUE
+	#define QUEUE
+	#define NAME_LENGTH 256
+	char **create_queue(size_t);
+	int isfull();
+	int isempty();
+	int enqueue(const char* filename);
+	const char *dequeue();
+#endif
