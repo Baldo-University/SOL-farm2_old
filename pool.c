046aeb14e9bdd1bb83750b9c2237f0fa4c5984a5
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-05-14 13:42:08 -0400

    Add files via upload

--- pool.c
+++ pool.c
@@ -1,11 +1,53 @@
+#include <pthread.h>
 #include <stdio.h>
+#include "pool.h"
+#include "queue.h"
+
+#define ec_null(s,m) \
+	if((s)==NULL) {perror(m); exit(EXIT_FAILURE);}
 
 /*
 Implementazione del threadpool
+Composto da una lista di thread, 
 */
 
-/*TODO
+//Struct per indicare la lista di thread del threadpool
+typedef struct {
+	pthread_t thread;	//thread
+	struct threadnode *next;	//puntatore al prossimo thread
+} threadnode;
+typedef struct threadnode* threadlist;	//definizione aggiuntiva per rendere piu' chiaro il codice
 
-*/
+//Struct del threadpool
+typedef struct {
+	threadlist threads_head;	//puntatore alla testa di lista thread
+	threadlist threads_rear;	//puntatore alla coda di lista thread
+	size_t num_threads;		//numero di thread
+} threadpool;
 
-void 
+extern int threadpool_running=0;	//indica se il threadpool sia attivo o meno
+extern pthread_mutex_t poolmutex;	//mutex del threadpool
+
+threadpool * create_pool(size_t size) {
+	threadpool *pool;
+	ec_null(pool=(threadpool*)malloc(sizeof(threadpool)),"pool, s.c. allocazione memoria al threadpool");
+	pool->num_threads=size;
+	pthread_mutex_init(&poolmutex,NULL);
+	threadpool_running=1;
+	return pool;
+}
+
+/*Aggiunta di un nuovo thread*/
+int add_thread(threadpool*) {
+
+}
+
+/*Rimozione di un thread*/
+void remove_thread(threadpool*) {
+
+}
+
+/*Terminazione del threadpool*/
+void close_pool(threadpool*) {
+
+}
--- pool.h
+++ pool.h
@@ -3,11 +3,17 @@
 #define THREADPOOL
 
 typedef struct threadpool threadpool;
+typedef struct threadnode threadnode;
+typedef struct threadlist threadlist;
 
 /*Creazione di un nuovo threadpool*/
-threadpool * create_pool(size_t);	
+//argomenti passati: numero iniziale di thread, coda di produzione
+//returns: threadpool
+threadpool * create_pool(size_t,char**);	
 
 /*Aggiunta di un nuovo thread*/
+//argomenti passati:
+//returns: 
 int add_thread(threadpool*);
 
 /*Rimozione di un thread*/
